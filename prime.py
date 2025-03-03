3c54f0de059ebbeb57e154a1f8aa2ef7d5644a06
Bilal <a.bilal292001@gmail.com>
2025-03-03 15:39:30 +0530

    03-3-2025 Version 1

--- prime.py
+++ prime.py
@@ -1,15 +1,21 @@
-# find the given number is prime or not
+# find the given number is prime or not return accordingly
 
 import math
 import time
 
 def Is_prime(num):
 
-  if num<=1:
-    return False
-  else:
-    return True
 
-num = int(input(("Enter a number:")))
-print(type(num))
-print(Is_prime(num))
+  if num<=1:
+    return "Not a prime"
+  
+  for i in range(2,int(math.sqrt(num)+1)): # sqrt will retuen float so do convert it nto int()
+    if num % i == 0:         # Point to note is dont return or assume as soon as this condition fails then the num must be prime and 
+      return "Not a Prime"   # dont write a else block here!!! the entire for loop must be failed for this 
+  return "Prime"             
+  
+t0 = time.time()
+for i in range(1,100001):
+  print(i , Is_prime(i))
+t1 = time.time()
+print(t1-t0)
